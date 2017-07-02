dc3e2612b8de901174fa573fc3fdeca77bc33a5c
Andrew Hart <Andrew@ProjectDent.com>
2017-07-03 00:44:33 +0100

    setup scene anchor, euler angles set to face north

--- ARKit+CoreLocation/Source/SceneLocationView.swift
+++ ARKit+CoreLocation/Source/SceneLocationView.swift
@@ -10,6 +10,10 @@ import Foundation
 import ARKit
 import CoreLocation
 
+fileprivate class SceneAnchor: ARAnchor {
+    var node: SCNNode?
+}
+
 class SceneLocationView: UIView, ARSCNViewDelegate, LocationManagerDelegate {
     private let sceneView = ARSCNView()
     
@@ -17,13 +21,13 @@ class SceneLocationView: UIView, ARSCNViewDelegate, LocationManagerDelegate {
     
     private(set) var sceneAnnotations = [SceneAnnotation]()
     
+    private var sceneAnchor: SceneAnchor?
+    
     //MARK: Setup
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         locationManager.delegate = self
-        
-        backgroundColor = UIColor.red
         
         sceneView.delegate = self
         
@@ -59,8 +63,46 @@ class SceneLocationView: UIView, ARSCNViewDelegate, LocationManagerDelegate {
         sceneView.frame = self.bounds
     }
     
+    func setupSceneAnchor() {
+        //Heading will be required to setup the node properly
+        if let currentFrame = self.sceneView.session.currentFrame,
+            self.locationManager.heading != nil {
+            let translation = matrix_identity_float4x4
+            let transform = simd_mul(currentFrame.camera.transform, translation)
+            self.sceneAnchor = SceneAnchor(transform: transform)
+            
+            //Add anchor to scene view
+            self.sceneView.session.add(anchor: self.sceneAnchor!)
+        }
+    }
+    
     //MARK: ARSCNViewDelegate
     
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        if let sceneAnchor = anchor as? SceneAnchor {
+            if let heading = self.locationManager.heading {
+                print("did update node for scene anchor")
+                node.eulerAngles.x -= Float(heading).degreesToRadians
+                node.eulerAngles.y = 0
+                node.eulerAngles.z = 0 - (Float.pi/2)
+                
+                //An arrow that points north
+//                let scene = SCNScene(named: "art.scnassets/arrow.dae")!
+//                let arrowNode = scene.rootNode.childNode(withName: "SketchUp", recursively: true)!
+//                arrowNode.scale = SCNVector3(x: 0.01, y: 0.01, z: 0.01)
+//                arrowNode.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+//                node.addChildNode(arrowNode)
+                
+                sceneAnchor.node = node
+            }
+        }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRenderScene scene: SCNScene, atTime time: TimeInterval) {
+        if sceneAnchor == nil {
+            self.setupSceneAnchor()
+        }
+    }
     
     //MARK: LocationManager
     
