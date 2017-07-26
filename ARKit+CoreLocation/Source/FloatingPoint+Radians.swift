d9504593c6e027257981302f1687d0cd87cca884
Andrew Hart <Andrew@ProjectDent.com>
2017-07-26 11:51:26 +0100

    floating point extensions become public

--- ARKit+CoreLocation/Source/FloatingPoint+Radians.swift
+++ ARKit+CoreLocation/Source/FloatingPoint+Radians.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-extension FloatingPoint {
-    var degreesToRadians: Self { return self * .pi / 180 }
-    var radiansToDegrees: Self { return self * 180 / .pi }
+public extension FloatingPoint {
+    public var degreesToRadians: Self { return self * .pi / 180 }
+    public var radiansToDegrees: Self { return self * 180 / .pi }
 }
