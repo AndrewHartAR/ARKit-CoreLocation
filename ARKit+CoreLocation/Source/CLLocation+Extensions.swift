84c8b8111b84f8abaa314de0589d50530107fdc4
Andrew Hart <Andrew@ProjectDent.com>
2017-07-27 09:51:57 +0100

    core location extensions and location translations become public

--- ARKit+CoreLocation/Source/CLLocation+Extensions.swift
+++ ARKit+CoreLocation/Source/CLLocation+Extensions.swift
@@ -10,10 +10,16 @@ import Foundation
 import CoreLocation
 
 ///Translation in meters between 2 locations
-struct LocationTranslation {
-    var latitudeTranslation: Double
-    var longitudeTranslation: Double
-    var altitudeTranslation: Double
+public struct LocationTranslation {
+    public var latitudeTranslation: Double
+    public var longitudeTranslation: Double
+    public var altitudeTranslation: Double
+    
+    public init(latitudeTranslation: Double, longitudeTranslation: Double, altitudeTranslation: Double) {
+        self.latitudeTranslation = latitudeTranslation
+        self.longitudeTranslation = longitudeTranslation
+        self.altitudeTranslation = altitudeTranslation
+    }
 }
 
 public extension CLLocation {
@@ -23,7 +29,7 @@ public extension CLLocation {
     
     ///Translates distance in meters between two locations.
     ///Returns the result as the distance in latitude and distance in longitude.
-    func translation(toLocation location: CLLocation) -> LocationTranslation {
+    public func translation(toLocation location: CLLocation) -> LocationTranslation {
         let inbetweenLocation = CLLocation(latitude: self.coordinate.latitude, longitude: location.coordinate.longitude)
         
         let distanceLatitude = location.distance(from: inbetweenLocation)
@@ -54,7 +60,7 @@ public extension CLLocation {
             altitudeTranslation: altitudeTranslation)
     }
     
-    func translatedLocation(with translation: LocationTranslation) -> CLLocation {
+    public func translatedLocation(with translation: LocationTranslation) -> CLLocation {
         let latitudeCoordinate = self.coordinate.coordinateWithBearing(bearing: 0, distanceMeters: translation.latitudeTranslation)
         
         let longitudeCoordinate = self.coordinate.coordinateWithBearing(bearing: 90, distanceMeters: translation.longitudeTranslation)
@@ -79,8 +85,8 @@ extension Double {
     }
 }
 
-extension CLLocationCoordinate2D {
-    func coordinateWithBearing(bearing:Double, distanceMeters:Double) -> CLLocationCoordinate2D {
+public extension CLLocationCoordinate2D {
+    public func coordinateWithBearing(bearing:Double, distanceMeters:Double) -> CLLocationCoordinate2D {
         //The numbers for earth radius may be _off_ here
         //but this gives a reasonably accurate result..
         //Any correction here is welcome.
