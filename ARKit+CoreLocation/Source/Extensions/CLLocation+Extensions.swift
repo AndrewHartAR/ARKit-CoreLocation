cca94034db596b54b5e2dd38ccf4686a4197725a
Ilya Seliverstov <ilya@amudo.com>
2017-08-11 16:31:03 +0200

    draw routs in scene

--- ARKit+CoreLocation/Source/Extensions/CLLocation+Extensions.swift
+++ ARKit+CoreLocation/Source/Extensions/CLLocation+Extensions.swift
@@ -60,6 +60,20 @@ public extension CLLocation {
                           verticalAccuracy: self.verticalAccuracy,
                           timestamp: self.timestamp)
     }
+
+    func bearing(between point: CLLocation) -> Double {
+        let lat1 = self.coordinate.latitude.degreesToRadians
+        let lon1 = self.coordinate.longitude.degreesToRadians
+
+        let lat2 = point.coordinate.latitude.degreesToRadians
+        let lon2 = point.coordinate.longitude.degreesToRadians
+
+        let dLon = lon2 - lon1
+
+        let y = sin(dLon) * cos(lat2)
+        let x = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(dLon)
+        return atan2(y, x).radiansToDegrees
+    }
 }
 
 extension CLLocation {
