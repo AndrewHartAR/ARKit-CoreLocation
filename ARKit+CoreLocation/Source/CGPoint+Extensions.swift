7716db6a9e3591a423a44d3ce1e5597acb7c4897
Aaron Brethorst <aaron@brethorsting.com>
2018-05-29 00:10:21 -0700

    Adds Swiftlint support
    Adds a very basic set of Swiftlint rules, and makes sure that the project and demo app follow them. More details on Swiftlint are available at https://github.com/realm/SwiftLint

--- ARKit+CoreLocation/Source/CGPoint+Extensions.swift
+++ ARKit+CoreLocation/Source/CGPoint+Extensions.swift
@@ -13,12 +13,12 @@ extension CGPoint {
     static func pointWithVector(vector: SCNVector3) -> CGPoint {
         return CGPoint(x: CGFloat(vector.x), y: CGFloat(0 - vector.z))
     }
-    
+
     func radiusContainsPoint(radius: CGFloat, point: CGPoint) -> Bool {
         let x = pow(point.x - self.x, 2)
         let y = pow(point.y - self.y, 2)
         let radiusSquared = pow(radius, 2)
-        
+
         return x + y <= radiusSquared
     }
 }
--- ARKit+CoreLocation/ViewController.swift
+++ ARKit+CoreLocation/ViewController.swift
@@ -7,79 +7,79 @@
 //
 
 import UIKit
-import SceneKit 
+import SceneKit
 import MapKit
 
 @available(iOS 11.0, *)
 class ViewController: UIViewController, MKMapViewDelegate, SceneLocationViewDelegate {
     let sceneLocationView = SceneLocationView()
-    
+
     let mapView = MKMapView()
     var userAnnotation: MKPointAnnotation?
     var locationEstimateAnnotation: MKPointAnnotation?
-    
+
     var updateUserLocationTimer: Timer?
-    
+
     ///Whether to show a map view
     ///The initial value is respected
     var showMapView: Bool = false
-    
+
     var centerMapOnUserLocation: Bool = true
-    
+
     ///Whether to display some debugging data
     ///This currently displays the coordinate of the best location estimate
     ///The initial value is respected
     var displayDebugging = false
-    
+
     var infoLabel = UILabel()
-    
+
     var updateInfoLabelTimer: Timer?
-    
+
     var adjustNorthByTappingSidesOfScreen = false
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         infoLabel.font = UIFont.systemFont(ofSize: 10)
         infoLabel.textAlignment = .left
         infoLabel.textColor = UIColor.white
         infoLabel.numberOfLines = 0
         sceneLocationView.addSubview(infoLabel)
-        
+
         updateInfoLabelTimer = Timer.scheduledTimer(
             timeInterval: 0.1,
             target: self,
             selector: #selector(ViewController.updateInfoLabel),
             userInfo: nil,
             repeats: true)
-        
+
         //Set to true to display an arrow which points north.
         //Checkout the comments in the property description and on the readme on this.
 //        sceneLocationView.orientToTrueNorth = false
-        
+
 //        sceneLocationView.locationEstimateMethod = .coreLocationDataOnly
         sceneLocationView.showAxesNode = true
         sceneLocationView.locationDelegate = self
-        
+
         if displayDebugging {
             sceneLocationView.showFeaturePoints = true
         }
-        
+
         //Currently set to Canary Wharf
         let pinCoordinate = CLLocationCoordinate2D(latitude: 51.504607, longitude: -0.019592)
         let pinLocation = CLLocation(coordinate: pinCoordinate, altitude: 236)
         let pinImage = UIImage(named: "pin")!
         let pinLocationNode = LocationAnnotationNode(location: pinLocation, image: pinImage)
         sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: pinLocationNode)
-        
+
         view.addSubview(sceneLocationView)
-        
+
         if showMapView {
             mapView.delegate = self
             mapView.showsUserLocation = true
             mapView.alpha = 0.8
             view.addSubview(mapView)
-            
+
             updateUserLocationTimer = Timer.scheduledTimer(
                 timeInterval: 0.5,
                 target: self,
@@ -87,97 +87,96 @@ class ViewController: UIViewController, MKMapViewDelegate, SceneLocationViewDele
                 userInfo: nil,
                 repeats: true)
         }
-        
+
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         print("run")
         sceneLocationView.run()
     }
-    
+
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
+
         print("pause")
         // Pause the view's session
         sceneLocationView.pause()
     }
-    
+
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
+
         sceneLocationView.frame = CGRect(
             x: 0,
             y: 0,
             width: self.view.frame.size.width,
             height: self.view.frame.size.height)
-        
+
         infoLabel.frame = CGRect(x: 6, y: 0, width: self.view.frame.size.width - 12, height: 14 * 4)
-        
+
         if showMapView {
             infoLabel.frame.origin.y = (self.view.frame.size.height / 2) - infoLabel.frame.size.height
         } else {
             infoLabel.frame.origin.y = self.view.frame.size.height - infoLabel.frame.size.height
         }
-        
+
         mapView.frame = CGRect(
             x: 0,
             y: self.view.frame.size.height / 2,
             width: self.view.frame.size.width,
             height: self.view.frame.size.height / 2)
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-    
+
     @objc func updateUserLocation() {
         if let currentLocation = sceneLocationView.currentLocation() {
             DispatchQueue.main.async {
-                
+
                 if let bestEstimate = self.sceneLocationView.bestLocationEstimate(),
                     let position = self.sceneLocationView.currentScenePosition() {
                     print("")
                     print("Fetch current location")
                     print("best location estimate, position: \(bestEstimate.position), location: \(bestEstimate.location.coordinate), accuracy: \(bestEstimate.location.horizontalAccuracy), date: \(bestEstimate.location.timestamp)")
                     print("current position: \(position)")
-                    
+
                     let translation = bestEstimate.translatedLocation(to: position)
-                    
+
                     print("translation: \(translation)")
                     print("translated location: \(currentLocation)")
                     print("")
                 }
-                
+
                 if self.userAnnotation == nil {
                     self.userAnnotation = MKPointAnnotation()
                     self.mapView.addAnnotation(self.userAnnotation!)
                 }
-                
+
                 UIView.animate(withDuration: 0.5, delay: 0, options: UIViewAnimationOptions.allowUserInteraction, animations: {
                     self.userAnnotation?.coordinate = currentLocation.coordinate
                 }, completion: nil)
-            
+
                 if self.centerMapOnUserLocation {
                     UIView.animate(withDuration: 0.45, delay: 0, options: UIViewAnimationOptions.allowUserInteraction, animations: {
                         self.mapView.setCenter(self.userAnnotation!.coordinate, animated: false)
-                    }, completion: {
-                        _ in
+                    }, completion: { _ in
                         self.mapView.region.span = MKCoordinateSpan(latitudeDelta: 0.0005, longitudeDelta: 0.0005)
                     })
                 }
-                
+
                 if self.displayDebugging {
                     let bestLocationEstimate = self.sceneLocationView.bestLocationEstimate()
-                    
+
                     if bestLocationEstimate != nil {
                         if self.locationEstimateAnnotation == nil {
                             self.locationEstimateAnnotation = MKPointAnnotation()
                             self.mapView.addAnnotation(self.locationEstimateAnnotation!)
                         }
-                        
+
                         self.locationEstimateAnnotation!.coordinate = bestLocationEstimate!.location.coordinate
                     } else {
                         if self.locationEstimateAnnotation != nil {
@@ -189,39 +188,39 @@ class ViewController: UIViewController, MKMapViewDelegate, SceneLocationViewDele
             }
         }
     }
-    
+
     @objc func updateInfoLabel() {
         if let position = sceneLocationView.currentScenePosition() {
             infoLabel.text = "x: \(String(format: "%.2f", position.x)), y: \(String(format: "%.2f", position.y)), z: \(String(format: "%.2f", position.z))\n"
         }
-        
+
         if let eulerAngles = sceneLocationView.currentEulerAngles() {
             infoLabel.text!.append("Euler x: \(String(format: "%.2f", eulerAngles.x)), y: \(String(format: "%.2f", eulerAngles.y)), z: \(String(format: "%.2f", eulerAngles.z))\n")
         }
-        
+
         if let heading = sceneLocationView.locationManager.heading,
             let accuracy = sceneLocationView.locationManager.headingAccuracy {
             infoLabel.text!.append("Heading: \(heading)º, accuracy: \(Int(round(accuracy)))º\n")
         }
-        
+
         let date = Date()
         let comp = Calendar.current.dateComponents([.hour, .minute, .second, .nanosecond], from: date)
-        
+
         if let hour = comp.hour, let minute = comp.minute, let second = comp.second, let nanosecond = comp.nanosecond {
             infoLabel.text!.append("\(String(format: "%02d", hour)):\(String(format: "%02d", minute)):\(String(format: "%02d", second)):\(String(format: "%03d", nanosecond / 1000000))")
         }
     }
-    
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
-        
+
         if let touch = touches.first {
             if touch.view != nil {
-                if (mapView == touch.view! ||
-                    mapView.recursiveSubviews().contains(touch.view!)) {
+                if mapView == touch.view! ||
+                    mapView.recursiveSubviews().contains(touch.view!) {
                     centerMapOnUserLocation = false
                 } else {
-                    
+
                     let location = touch.location(in: self.view)
 
                     if location.x <= 40 && adjustNorthByTappingSidesOfScreen {
@@ -240,17 +239,17 @@ class ViewController: UIViewController, MKMapViewDelegate, SceneLocationViewDele
             }
         }
     }
-    
-    //MARK: MKMapViewDelegate
-    
+
+    // MARK: - MKMapViewDelegate
+
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         if annotation is MKUserLocation {
             return nil
         }
-        
+
         if let pointAnnotation = annotation as? MKPointAnnotation {
             let marker = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: nil)
-            
+
             if pointAnnotation == self.userAnnotation {
                 marker.displayPriority = .required
                 marker.glyphImage = UIImage(named: "user")
@@ -259,32 +258,32 @@ class ViewController: UIViewController, MKMapViewDelegate, SceneLocationViewDele
                 marker.markerTintColor = UIColor(hue: 0.267, saturation: 0.67, brightness: 0.77, alpha: 1.0)
                 marker.glyphImage = UIImage(named: "compass")
             }
-            
+
             return marker
         }
-        
+
         return nil
     }
-    
-    //MARK: SceneLocationViewDelegate
-    
+
+    // MARK: - SceneLocationViewDelegate
+
     func sceneLocationViewDidAddSceneLocationEstimate(sceneLocationView: SceneLocationView, position: SCNVector3, location: CLLocation) {
         print("add scene location estimate, position: \(position), location: \(location.coordinate), accuracy: \(location.horizontalAccuracy), date: \(location.timestamp)")
     }
-    
+
     func sceneLocationViewDidRemoveSceneLocationEstimate(sceneLocationView: SceneLocationView, position: SCNVector3, location: CLLocation) {
         print("remove scene location estimate, position: \(position), location: \(location.coordinate), accuracy: \(location.horizontalAccuracy), date: \(location.timestamp)")
     }
-    
+
     func sceneLocationViewDidConfirmLocationOfNode(sceneLocationView: SceneLocationView, node: LocationNode) {
     }
-    
+
     func sceneLocationViewDidSetupSceneNode(sceneLocationView: SceneLocationView, sceneNode: SCNNode) {
-        
+
     }
-    
+
     func sceneLocationViewDidUpdateLocationAndScaleOfLocationNode(sceneLocationView: SceneLocationView, locationNode: LocationNode) {
-        
+
     }
 }
 
@@ -298,11 +297,11 @@ extension DispatchQueue {
 extension UIView {
     func recursiveSubviews() -> [UIView] {
         var recursiveSubviews = self.subviews
-        
+
         for subview in subviews {
             recursiveSubviews.append(contentsOf: subview.recursiveSubviews())
         }
-        
+
         return recursiveSubviews
     }
 }
