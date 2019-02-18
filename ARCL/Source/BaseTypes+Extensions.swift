b9ed5f6877a1e17efab41e26d1b8332283c47e72
Eric Internicola <intere@gmail.com>
2019-02-18 09:08:39 -0700

    Added missing file.

---
+++ ARCL/Source/BaseTypes+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  BaseTypes+Extensions.swift
+//  ARKit+CoreLocation
+//
+//  Created by Ilya Seliverstov on 08/08/2017.
+//  Copyright © 2017 Project Dent. All rights reserved.
+//
+ import Foundation
+
+ extension Float {
+    var short: String { return String(format: "%.2f", self) }
+}
+
+ extension Int {
+    var short: String { return String(format: "%02d", self) }
+    var short3: String { return String(format: "%03d", self / 1_000_000) }
+}
+
