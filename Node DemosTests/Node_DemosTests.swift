86f5069bec8fdbcc0bac8ebb0af2999bc3a19f47
Hal Mueller <hal@mobilegeographics.com>
2019-10-08 11:46:22 -0700

    Fix test class names.

--- Node DemosTests/Node_DemosTests.swift
+++ Node DemosTests/Node_DemosTests.swift
@@ -9,7 +9,7 @@
 import XCTest
 @testable import Node_Demos
 
-class Node_DemosTests: XCTestCase {
+class NodeDemosTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
--- Node DemosUITests/Node_DemosUITests.swift
+++ Node DemosUITests/Node_DemosUITests.swift
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class Node_DemosUITests: XCTestCase {
+class NodeDemosUITests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
