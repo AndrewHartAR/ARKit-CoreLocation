8fe832eeab8340b8e29c7f0398b544bf02b02946
Hal Mueller <hal@mobilegeographics.com>
2019-11-25 14:22:30 -0800

    run "pod install" to enable Node Demos to build out of the box

---
+++ Pods/Target Support Files/Pods-Node Demos-Node DemosTests/Pods-Node Demos-Node DemosTests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Node_Demos_Node_DemosTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Node_Demos_Node_DemosTestsVersionString[];
+
